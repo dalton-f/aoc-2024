488e80ce022c272e2898c2c14687d16ec513f803
Dalton <daltonfiggdavies@gmail.com>
2024-12-06 15:47:15 +0000

    Slightly more optimised Day 6 Part 2

--- days/day-06/part-2.js
+++ days/day-06/part-2.js
@@ -7,7 +7,10 @@ const fs = require("fs");
 
 const text = fs.readFileSync("days/day-06/input.txt", "utf-8");
 
-const map = text.trim().split("\n");
+const map = text
+  .trim()
+  .split("\n")
+  .map((row) => row.split(""));
 
 // Save variable definitions
 const obstruction = "#";
@@ -25,12 +28,10 @@ const directions = [
 let guardRow = 0;
 let guardCol = 0;
 
-let potentialObstructions = 0;
-
 // Loops over the entire map to fetch the i, j coordinates of the guard
 
 for (let i = 0; i < map.length; i++) {
-  for (let j = 0; j < map[0].length; j++) {
+  for (let j = 0; j < map[i].length; j++) {
     if (map[i][j] === guardSymbol) {
       guardRow = i;
       guardCol = j;
@@ -39,64 +40,104 @@ for (let i = 0; i < map.length; i++) {
   }
 }
 
+// Save initial position
 let startingRow = guardRow;
 let startingCol = guardCol;
 
-// Loop over all squares and test if it loops with an  obstruction in it
-for (let i = 0; i < map.length; i++) {
-  for (let j = 0; j < map[i].length; j++) {
-    // Test all squares as obstructions
-    map[i][j] = obstruction;
+let direction = 0;
 
-    // Locally reset the values
-    let direction = 0;
+// Track distinct visited positions using a set to save on additional checks
+const visited = new Set();
+visited.add(`${guardRow},${guardCol}`);
 
-    guardRow = startingRow;
-    guardCol = startingCol;
+while (true) {
+  const nextRow = guardRow + directions[direction][0];
+  const nextCol = guardCol + directions[direction][1];
 
-    // Track distinct visited positions using a set to save on additional checks
-    const visited = new Set();
-
-    while (true) {
-      const nextRow = guardRow + directions[direction][0];
-      const nextCol = guardCol + directions[direction][1];
-
-      // If visited with the same direction, it is a loop
-      if (visited.has(`${guardRow},${guardCol},${directions[direction]}`)) {
-        potentialObstructions++;
-        // Always return the map to its previous state
-        map[i][j] === ".";
-        break;
-      }
-
-      // Add visited square before direction changes
-      visited.add(`${guardRow},${guardCol},${directions[direction]}`);
-
-      // If the guard has moved out of the map, stop searching
-      if (
-        nextRow < 0 ||
-        nextRow >= map.length ||
-        nextCol < 0 ||
-        nextCol >= map[0].length
-      ) {
-        // Always return the map to its previous state
-        map[i][j] === ".";
-        break;
-      }
-
-      // Rotate by 90 degrees if the next part of the map is obstructed
-      if (
-        map[nextRow][nextCol] === obstruction ||
-        (nextCol === i && nextRow === j)
-      ) {
-        direction = (direction + 1) % 4;
-      } else {
-        // Move forward
-        guardRow = nextRow;
-        guardCol = nextCol;
-      }
-    }
+  // If the guard has moved out of the map, stop searching
+  if (
+    nextRow < 0 ||
+    nextRow >= map.length ||
+    nextCol < 0 ||
+    nextCol >= map[0].length
+  ) {
+    break;
   }
+
+  // Rotate by 90 degrees if the next part of the map is obstructed
+  if (map[nextRow][nextCol] === obstruction) {
+    direction = (direction + 1) % 4;
+  } else {
+    // Move forward
+    guardRow = nextRow;
+    guardCol = nextCol;
+  }
+
+  visited.add(`${guardRow},${guardCol}`);
 }
 
-console.log(potentialObstructions);
+// Using part 1 path simulate to generate all distinct squares that the guard will be on
+// We can use ths to brute force a cycle detection in a more efficent way than testing all squares that the guard may never reach
+
+let potentialObstacles = 0;
+
+// Similar traversal to part one
+const isCyclic = (row, col) => {
+  if (map[row][col] === obstruction || map[row][col] === guardSymbol)
+    return false;
+
+  // Ensure to reset the values
+  direction = 0;
+
+  guardRow = startingRow;
+  guardCol = startingCol;
+
+  // Check for an obstruction at a given index
+  map[row][col] = obstruction;
+
+  const visitedOrientations = new Set();
+
+  while (true) {
+    const nextRow = guardRow + directions[direction][0];
+    const nextCol = guardCol + directions[direction][1];
+
+    // We can detect a loop if we reach the same row, col and orientation more than once
+
+    if (visitedOrientations.has(`${guardRow},${guardCol},${direction}`)) {
+      // Return map to its previous state
+      map[row][col] = ".";
+      return true;
+    }
+
+    visitedOrientations.add(`${guardRow},${guardCol},${direction}`);
+
+    // If the guard leaves the map, it is not stuck in a loop
+    if (
+      nextRow < 0 ||
+      nextRow >= map.length ||
+      nextCol < 0 ||
+      nextCol >= map[0].length
+    ) {
+      // Return map to its previous state
+      map[row][col] = ".";
+      return false;
+    }
+
+    // Rotate by 90 degrees if the next part of the map is obstructed
+    if (map[nextRow][nextCol] === obstruction) {
+      direction = (direction + 1) % 4;
+    } else {
+      // Move forward
+      guardRow = nextRow;
+      guardCol = nextCol;
+    }
+  }
+};
+
+for (const position of visited) {
+  const [row, col] = position.split(",");
+
+  if (isCyclic(row, col)) potentialObstacles++;
+}
+
+console.log(potentialObstacles);
